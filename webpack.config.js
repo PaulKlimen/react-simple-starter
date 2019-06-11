9689cee386bd5f5f2ac5d30ec86f287b57913e07
Pavel Klimenko <kp@kodix.ru>
2019-06-11 04:02:10 +0300

    fix config

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
 
   mode: 'development',
